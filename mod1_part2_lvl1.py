95d683f76b6e0f9225d8c0443172b4104b016d9e
dsejm <dsejmovskij@gmail.com>
2023-06-17 15:33:12 +0300

    correction file

--- mod1_part2_lvl1.py
+++ mod1_part2_lvl1.py
@@ -1,7 +1,7 @@
 num_1 = int(input("Введите 1 число: "))
 num_2 = int(input("Введите 2 число: "))
 num_3 = int(input("Введите 3 число: "))
-if num_1 == num_2 & num_1 == num_3:
+if num_1 == num_2 and num_1 == num_3: # Заменяю символ "&" на "and"
     print("Совпадающих чисел: 3")
 elif num_1 == num_2 or num_1 == num_3:
     print("Совпадающих чисел: 2")
